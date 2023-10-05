a41eecd241825b73b8e4b2defbe47007eef90ad8
Piotr Staniszewski <p.stanisz@gmail.com>
2023-10-05 21:23:58 +0200

    asci art ui

--- hangman/main.py
+++ hangman/main.py
@@ -1,39 +1,50 @@
 #!/usr/bin/python3
 
 import random
+import os
+from resources import logo, stages, win
+from words import word_list
 
-word_list = ["Angel", "Devil", "Wizard", "Dragon"]
+print(logo)
+print(input("Press any key to start..."))
+os.system('clear')
 
 word = random.choice(word_list)
 word_len = len(word)
 
 hidden_word = []
 for _ in range(word_len):
-  hidden_word += "_"
-  
-print(hidden_word)
+    hidden_word += "_"
 
-guess_count = 10
+guess_count = len(stages) - 1
 all_letters_known = False
 
 while guess_count > 0 and not all_letters_known:
-
-    guess  = input("Guess a letter: ").lower()
+    print(hidden_word)
+    print("\n")
+    print(stages[guess_count])
+    
+    guess = input("Guess a letter: ").lower()
+    
+    os.system('clear')
 
     for position in range(word_len):
         letter = word[position]
         if letter.lower() == guess:
             hidden_word[position] = letter
-            
-    print(hidden_word)
-    
+
+    if guess not in word.lower():
+        guess_count -= 1
+
     if hidden_word.count("_") == 0:
         all_letters_known = True
-        
-    guess_count -= 1
-    
+
+print(hidden_word)
+print("\n")
+
 if all_letters_known:
+    print(win)
     print("You win :)")
 else:
+    print(stages[0])
     print("You lose :(")
-  
